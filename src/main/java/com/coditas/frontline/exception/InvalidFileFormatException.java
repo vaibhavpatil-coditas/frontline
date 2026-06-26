1a4e91869f22003c0b76012049abd72ff6fdf47f
vaibhavpatil-coditas <vaibhav.patil@coditas.com>
2026-06-26 14:49:15 +0530

    Trying to implement spring ai

---
+++ src/main/java/com/coditas/frontline/exception/InvalidFileFormatException.java
@@ -0,0 +1,7 @@
+package com.coditas.frontline.exception;
+
+public class InvalidFileFormatException extends RuntimeException {
+    public InvalidFileFormatException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/coditas/frontline/service/impl/SpringAiServiceImpl.java
+++ src/main/java/com/coditas/frontline/service/impl/SpringAiServiceImpl.java
@@ -5,21 +5,18 @@ import com.coditas.frontline.service.SpringAiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SpringAiServiceImpl implements SpringAiService {
-
-    private final OpenAiChatModel openAiChatModel;
     private final ChatClient chatClient;
     @Override
     public String testAi(SpringAiRequest request) {
-        String response = openAiChatModel.call(request.getTopic());
-        log.info("Ai response: {}", response);
-        String content = chatClient.prompt(request.getTopic()).call().content();
+        String content = chatClient.prompt(request.getTopic())
+                .call()
+                .content();
         log.info("Ai response: {}", content);
         return content;
     }
